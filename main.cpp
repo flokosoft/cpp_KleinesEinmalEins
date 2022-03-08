1db1ef20ad032f994d5da8743aa71c9d176ede21
Florian Koch <github@flokosoft.d>
2022-03-08 18:59:44 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int i2 = 1; i2 <= 10; i2++)
+        {
+            cout << i << "x" << i2 << " = " <<
+                      i*i2 << " || ";
+        }
+        cout << endl << endl;
+    }
+}
